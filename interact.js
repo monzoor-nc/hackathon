cf491bbc26b88d2469489c8e082beb3d11b3a93f
anam <anam.ahmed.a@gmail.com>
2014-09-13 16:50:40 +0600

    added random

--- interact.js
+++ interact.js
@@ -1,13 +1,16 @@
 (function(){
+	var colors = "#4c89c5,#50aa0e,#BE90D4,#87D37C,#EB9532,#BF55EC".split(',');
+	console.log(colors);
 $('.button').click(function(){
+var realmdel= $('.markerdetail').get(0);
 var dropbox;
 var current=null;
-var realmdel= $('.markerdetail').get(0);
 var addMarker = function(x,y){
 	var marker = document.createElement('div');
 	marker.className="marker";
 	marker.style.left=x-10; //assuming a 10x10 marker
 	marker.style.top=y-10;
+	marker.style.backgroundColor=colors[Math.ceil(Math.random()*5)];
 	$(marker).attr('data-detail','');
 	document.querySelector('body').appendChild(marker);
 	showDetailWindow(marker);
@@ -28,12 +31,15 @@ var showDetailWindow = function(el){
 $('.markerdetail').get(0).addEventListener('blur',function(){
 	if(!this.value){
 		$('body').get(0).removeChild(current);
+		$(this).hide();
 	}
 	else{
 	$(current).attr('data-detail',$(this).val());
 	$(this).hide();
 	}
 	current=null;
+	//checkmarkers();
+
 });
 var img = $('.clientimage');
 if(dropbox = $('.dropboxurl').val()){
@@ -49,5 +55,10 @@ if(dropbox = $('.dropboxurl').val()){
 		addMarker(x,y);
 		}
 	});
+});
+document.addEventListener('keydown',function(e){
+	if(e.keyCode==27){
+		document.querySelector('.markerdetail').blur();
+	}
 });
 })();
